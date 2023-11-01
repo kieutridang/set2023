edb453a222a574c64e7c4608aae6fcdfb226b094
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-11-01 21:26:58 +0700

    S231-11 | Implement Mock function

---
+++ team1/chessGame/frontend/scripts/account.js
@@ -0,0 +1,15 @@
+function signIn(username, password) {
+    //compare username and password on database
+    //if (sucesss) {
+    //  -> homepage
+    //}
+    //else {
+    //  -> invalid login
+    //}
+    return 
+}
+
+function signUp(username, password) {
+    //create new player
+    //save to database
+}
--- team1/chessGame/frontend/scripts/board.js
+++ team1/chessGame/frontend/scripts/board.js
@@ -1,3 +1,29 @@
-function test() {
-    console.log("test")
+function renderChessBoard(board) {
+    //render chess board 8x8
+    //for {
+        //render piece base on position
+        //if else
+        //0x0 -> black rook
+    //}
+}
+
+function showValidateMove(piece) {
+    //var result = validateMove(piece) ? true : false;
+    //return result;
+}
+
+function validateMove(piece, from, to) {
+    //if(piece === king) {
+    //   if (check(piece)) {
+    //      move king
+    //   } 
+    //}
+    //else {
+    // move piece
+    //}
+    return Boolean;
+}
+
+function showMoveHistory() {
+    // show move history
 }
