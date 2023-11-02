109d7e128e7b7b21e5964bb0f573c85d2a9001f2
Tran Quoc Trung <trung.tran-quoc@internal.banvien.com>
2023-11-02 16:42:34 +0700

    add render piece

---
+++ chessGame/client/untils/untils.js
@@ -0,0 +1,46 @@
+export const infoPiece = {
+    name: "",
+    position: 12,
+    color: "white"
+};
+
+
+
+export const arrInfoPieces = [
+    [
+        {
+            name: "pawn",
+            color: "white",
+            position: 12
+        },
+
+        {
+            name: "rook",
+            position: 13
+        },
+    
+        {
+            name: "knight",
+            position: 15
+        },
+    
+        {
+            name: "bishop",
+            position: 19
+        },
+    
+        {   
+            name: "queen",
+            position: 24
+        }
+    ],
+
+     [
+        {
+            name: "king",
+            position: 12
+        }
+    ]
+];
+
+// console.log(arrInfoPieces[idColor.WHITE][idPiece.BISHOP].name);
