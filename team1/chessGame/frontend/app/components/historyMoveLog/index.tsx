387d33e57216e330328e83f69096dcf954bbba54
nguyenhungne <nguyenhungf205@gmail.com>
2023-11-07 00:12:42 +0700

    fix css time stamp

--- team1/chessGame/frontend/app/components/historyMoveLog/index.tsx
+++ team1/chessGame/frontend/app/components/historyMoveLog/index.tsx
@@ -4,11 +4,11 @@ import styles from './styles.module.css';
 function HistoryMove() {
     return (
         <div className={styles.historyMove}>
-            <div className={styles.timeWhitePlayer}>
+            <div className={`${styles.timeWhitePlayer} ${styles.time}`}>
                 <h1>00:00</h1>
             </div>
            
-            <div className={styles.timeWhitePlayer}>
+            <div className={`${styles.timeBlackPlayer} ${styles.time}`}>
                 <h1>00:00</h1>
             </div>
         </div>
