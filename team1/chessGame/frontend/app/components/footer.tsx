0f982683c45fa0301c1c4954013777844b5d1d94
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-11-08 16:28:59 +0700

    Revert "design layout and main page"
    This reverts commit 087b6ad8495f9ec6b7f31ac11923207b0c0fcf14.

--- team1/chessGame/frontend/app/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-            <h1 className="footer__create">By team 1 SET 2023</h1>
-        </footer>
-    )
-}
