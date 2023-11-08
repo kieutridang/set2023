087b6ad8495f9ec6b7f31ac11923207b0c0fcf14
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-11-08 16:27:57 +0700

    design layout and main page

---
+++ team1/chessGame/frontend/app/components/header.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import React from 'react'
+
+export default function Header() {
+    return (
+        <head>
+            <meta charSet="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Document</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
+            <link rel="stylesheet" href="../styles/main.css"/>
+            <link rel="stylesheet" href="../styles/board.css"/>
+            <link rel="stylesheet" href="../styles/pieces.css"/>
+            <link rel="stylesheet" href="../styles/historyMove.css"/>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+            <link href="https://fonts.googleapis.com/css2?family=Alata&family=Quicksand:wght@300;400;500;600;700&display=swap"
+                rel="stylesheet" />
+        </head>
+    )
+}
