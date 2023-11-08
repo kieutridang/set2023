13055f97122d804804922d283a7fcc051313b00a
Trinh Viet Hoang <trinhviethoang16@gmail.com>
2023-11-08 16:38:52 +0700

    Auto stash before rebase of "refs/heads/develop_1"

--- team1/chessGame/frontend/app/layout.tsx
+++ team1/chessGame/frontend/app/layout.tsx
@@ -1,7 +1,20 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+<<<<<<< Updated upstream
 
+=======
+import "./styles/style.css";
+import Header from "./components/header";
+import './globals.css'
+import styles from './rootLayout.module.css'
+import Link from 'next/link'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { faChessBoard, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+config.autoAddCss = false; 
+>>>>>>> Stashed changes
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +29,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+<<<<<<< Updated upstream
       <body className={inter.className}>{children}</body>
+=======
+      <Header />
+      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+          <header className={styles.header}>
+            <Link href="./main" style={{textDecoration: "none"}}>
+              <FontAwesomeIcon icon={faChessBoard} className={styles.logoChessGame} />
+            </Link>
+            <div className={styles.nav}>
+                <Link className={styles.navAccounts} href="./sign-in" >Sign in</Link>
+                <Link className={styles.navAccounts} href="./sign-up" >Sign up</Link>
+                <FontAwesomeIcon icon={faEllipsisVertical} className={styles.navAccounts} />
+            </div>
+        </header>
+        {children}
+        <footer className={styles.footer}>
+             <h1 className={styles.footerCreate}>By team 1 SET 2023</h1>
+        </footer>
+      </body>
+>>>>>>> Stashed changes
     </html>
   )
 }
